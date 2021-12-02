3e3cab622be1e62327242aa43c7654b905ef19ae
satanja <s.a.tanja@student.tue.nl>
2021-12-02 20:52:16 +0100

    Add instance reading

---
+++ src/io.rs
@@ -0,0 +1,47 @@
+use crate::graph::Graph;
+use io::Result;
+use std::io;
+
+pub fn read() -> Result<Graph> {
+    let mut line = String::new();
+    let stdin = io::stdin();
+
+    stdin.read_line(&mut line)?;
+    while line.starts_with("%") {
+        line.clear();
+        stdin.read_line(&mut line)?;
+    }
+
+    let specs: Vec<_> = line
+        .clone()
+        .trim_end()
+        .split_whitespace()
+        .map(|v| v.parse::<usize>().unwrap())
+        .collect();
+    line.clear();
+
+    let vertices = specs[0];
+    let _edges = specs[1];
+
+    let mut graph = Graph::new(vertices);
+
+    let mut index = 0;
+    while index < vertices {
+        stdin.read_line(&mut line)?;
+        while line.starts_with("%") {
+            line.clear();
+            stdin.read_line(&mut line)?;
+        }
+        let adj: Vec<u32> = line
+            .clone()
+            .trim_end()
+            .split_whitespace()
+            .map(|v| v.parse::<u32>().unwrap() - 1)
+            .collect();
+        graph.set_adjacency(index as u32, adj);
+
+        line.clear();
+        index += 1;
+    }
+    Ok(graph)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+mod graph;
+mod io;
+use graph::Graph;
+use io::read;
+
 fn main() {
-    println!("Hello, world!");
+    let graph = read();
+
+    // let graph = Graph::new(4);
+
+    // graph.has_cycle(&vec![0, 2, 3]);
+
+    println!("sup!");
 }
