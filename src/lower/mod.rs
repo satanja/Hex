ae42d7537755af32d50b1e48d72ad503e44c9109
s.a.tanja <s.a.tanja@student.tue.nl>
2022-03-02 12:22:46 +0100

    Add cycle lower bound computation

--- src/lower/mod.rs
+++ src/lower/mod.rs
@@ -1,6 +1,7 @@
 //! Module to compute lower bounds for the graph
 use crate::graph::Graph;
 mod vc_rilp;
+mod cycle_rilp;
 
 pub fn lower_bound(graph: &Graph) -> usize {
     vc_rilp::lower_bound(graph)
