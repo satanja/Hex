42fcd43a3bc1699819cebafd71b1adc0b9fe0cff
satanja <s.a.tanja@student.tue.nl>
2022-04-29 11:29:47 +0200

    Compute better lower bounds

--- src/lower/mod.rs
+++ src/lower/mod.rs
@@ -1,12 +1,9 @@
 //! Module to compute lower bounds for the graph
 use crate::graph::{Graph, Undirected};
+mod ecc_rilp;
 mod cycle_rilp;
 mod vc_rilp;
 
 pub fn lower_bound(graph: &Graph) -> usize {
-    if graph.is_undirected() {
-        vc_rilp::lower_bound(graph)
-    } else {
-        cycle_rilp::lower_bound(graph)
-    }
+    ecc_rilp::lower_bound(graph)
 }
