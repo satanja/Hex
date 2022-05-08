aabc731fff326bdbcadfd7e7488af3af1c45b131
satanja <s.a.tanja@student.tue.nl>
2022-05-08 12:18:01 +0200

    Fix optil build script

--- build_optil.sh
+++ build_optil.sh
@@ -4,7 +4,6 @@ podman rm optilbin
 podman create --name optilbin localhost/optil:latest
 mkdir optil_target/
 rm optil_target/dfvstritus
-mkdir optil_target/extern/WeGotYouCovered
 cp extern/WeGotYouCovered/vc_solver optil_target/vc_solver
 podman cp optilbin:./target/release/dfvstritus optil_target/
 tar -czvf optil_target/dfvstritus.tgz -C optil_target/ dfvstritus vc_solver
